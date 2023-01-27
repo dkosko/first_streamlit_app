d9cb287073fa6b0d36adaf000802a16528e3b597
dkosko <75519035+dkosko@users.noreply.github.com>
2023-01-27 18:51:40 +0100

    Update streamlit_app.py
    update fruit smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.title('Build your own fruit smoothie')
+streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
